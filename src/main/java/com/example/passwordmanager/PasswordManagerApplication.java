a6ca98ff66792a0f8f19843f125098cf3ec71f3a
yiptsunho <thyipac@connect.ust.hk>
2022-12-21 18:00:38 +0800

    first commit

---
+++ src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
@@ -0,0 +1,17 @@
+package com.example.passwordmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordManagerApplication.class, args);
+	}
+
+	public String helloWorld(String[] args) {
+		return "Hello World";
+	}
+
+}
