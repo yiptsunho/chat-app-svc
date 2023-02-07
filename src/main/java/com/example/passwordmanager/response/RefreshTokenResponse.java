95066baa8aadd64c9d81c6db965f7949a284df46
yiptsunho <thyipac@connect.ust.hk>
2023-02-07 10:05:06 +0800

    comment refresh token request and response

---
+++ src/main/java/com/example/passwordmanager/response/RefreshTokenResponse.java
@@ -0,0 +1,16 @@
+//package com.example.passwordmanager.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class RefreshTokenResponse {
+//
+//    private String accessToken;
+//    private String refreshToken;
+//}
